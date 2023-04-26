a799635883dfe5c7e4f0c4f4ed7a51acf1297223
aswnss-m <aswnss.m@gmail.com>
2023-04-26 14:34:30 +0530

    home page almost donw

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -3,12 +3,23 @@ import Navbar from '../Components/Navbar';
 import FloatingButton from '../Components/FloatingButton';
 import Emergency from '../Components/Emergency';
 
+import "../Assets/Home.css"
+
 function Home() {
   return (
     <div className="container">
         <Navbar />
         <Emergency />
-        {/* <FloatingButton /> */}
+        <div className="homeContentContainer">
+        <div className="homeContent">
+          <p>Donate blood, save life</p>
+          <h2>Donate Blood And Inspire Others</h2>
+          <div className="exploreButton">
+            Explore
+          </div>
+        </div>
+        </div>
+        <FloatingButton />
     </div>
   )
 }
