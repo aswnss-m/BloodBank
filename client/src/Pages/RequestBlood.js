fe0a7ea7ac50621ca173a9451419a5589a3eae48
aswnss-m <aswnss.m@gmail.com>
2023-04-26 21:20:56 +0530

    something is not working

--- client/src/Pages/RequestBlood.js
+++ client/src/Pages/RequestBlood.js
@@ -4,6 +4,7 @@ import "../Assets/RequestBlood.css"
 function RequestBlood() {
   return (
     <div className="formContainer">
+        <h3>Request Blood</h3>
         <form action="#" method="post" className='requestForm'>
             <div className="formGroup">
                 <label htmlFor="patientName">
@@ -12,34 +13,55 @@ function RequestBlood() {
                 <input type="text" id='patientName' />
             </div>
             <div className="formGroup">
-                <label htmlFor="patientName">
-                    Patient Name :
+                <label htmlFor="patientAge">
+                    Age :
                 </label>
-                <input type="text" id='patientName' />
+                <input type="number" id='patientAge' inputMode='number'/>
             </div>
             <div className="formGroup">
-                <label htmlFor="patientName">
-                    Patient Name :
+                <label htmlFor="patientBloodGroup">
+                    Blood Group :
                 </label>
-                <input type="text" id='patientName' />
+                <select name="bloodGroup" id="patientBloodGroup" defaultValue={'A+'}>
+                    <option value="A+">A -ve</option>
+                    <option value="A+">A +ve</option>
+                    <option value="A+">AB +ve</option>
+                    <option value="A+">AB -ve</option>
+                    <option value="A+">B +ve</option>
+                    <option value="A+">B -ve</option>
+                    <option value="A+">O +ve</option>
+                    <option value="A+">O -ve</option>
+                </select>
             </div>
             <div className="formGroup">
-                <label htmlFor="patientName">
-                    Patient Name :
+                <label htmlFor="bloodUnits">
+                    No of Units :
                 </label>
-                <input type="text" id='patientName' />
+                <input type="text" id='bloodUnits' />
             </div>
             <div className="formGroup">
-                <label htmlFor="patientName">
-                    Patient Name :
+                <label htmlFor="hospitalLocation">
+                    Hospital Location :
                 </label>
-                <input type="text" id='patientName' />
+                <input type="text" id='hospitalLocation' />
             </div>
             <div className="formGroup">
-                <label htmlFor="patientName">
-                    Patient Name :
+                <label htmlFor="standeeName">
+                    Standee Name :
                 </label>
-                <input type="text" id='patientName' />
+                <input type="text" id='standeeName' />
+            </div>
+            <div className="formGroup">
+                <label htmlFor="standeeNumber">
+                    Standee Mobile Number :
+                </label>
+                <input type="text" id='standeeNumber' inputMode='number'/>
+            </div>
+            <div className="formGroup">
+                <label htmlFor="otp">
+                    Otp : 
+                </label>
+                <input type="text" id='otp' />
             </div>
         </form>
     </div>
