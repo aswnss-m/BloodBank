9dc92775569822344a3d074e133fb57c021f51d1
aswnss-m <aswnss.m@gmail.com>
2023-05-01 19:45:04 +0530

    variables created in request blood and working

--- client/src/Pages/RequestBlood.js
+++ client/src/Pages/RequestBlood.js
@@ -1,28 +1,49 @@
 import React from 'react'
 import "../Assets/RequestBlood.css"
+import { useState } from 'react'
 
 function RequestBlood() {
+
+
+    const [patientName,SetPatientName] = useState("");
+    const [patientAge,SetPatientAge] = useState("");
+    const [patientBloodGroup,SetPatientBloodGroup] = useState("");
+    const [patientBloodUnits,SetPatientBloodUnits] = useState("");
+    const [patientLocation,SetPatientLocation] = useState("");
+    const [standeeName,SetStandeeName] = useState("");
+    const [standeeNumber,SetStandeeNumber] = useState("");
+
+    const handleClick = () =>{
+        console.log(patientName);
+        console.log(patientAge);
+        console.log(patientBloodGroup);
+        console.log(patientBloodUnits);
+        console.log(patientLocation);
+        console.log(standeeName);
+        console.log(standeeNumber);
+    }
+
   return (
     <div className="formContainer">
-        <form action="#" method="post" className='form'>
+        <div className='form'>
         <h3 className='formTitle' >Request Blood</h3>
             <div className="formGroup">
                 <label htmlFor="patientName">
                     Patient Name :
                 </label>
-                <input type="text" id='patientName' autoFocus/>
+                <input type="text" id='patientName' autoFocus onChange={(e)=>{SetPatientName(e.target.value)}}/>
             </div>
             <div className="formGroup">
                 <label htmlFor="patientAge">
                     Age :
                 </label>
-                <input type="number" id='patientAge' inputMode='number'/>
+                <input type="number" id='patientAge' inputMode='number'onChange={(e)=>{SetPatientAge(e.target.value)}}/>
             </div>
             <div className="formGroup">
                 <label htmlFor="patientBloodGroup">
                     Blood Group :
                 </label>
-                <select name="bloodGroup" id="patientBloodGroup" defaultValue={'A+'}>
+                <select name="bloodGroup" id="patientBloodGroup" defaultValue={'A+'} onChange={(e)=>{SetPatientBloodGroup(e.target.value)}}>
                     <option value="A+">A -ve</option>
                     <option value="A+">A +ve</option>
                     <option value="A+">AB +ve</option>
@@ -37,27 +58,27 @@ function RequestBlood() {
                 <label htmlFor="bloodUnits">
                     No of Units :
                 </label>
-                <input type="text" id='bloodUnits' />
+                <input type="number" inputMode='number' id='bloodUnits' onChange={(e)=>{SetPatientBloodUnits(e.target.value)}}/>
             </div>
             <div className="formGroup">
                 <label htmlFor="hospitalLocation">
                     Hospital Location :
                 </label>
-                <input type="text" id='hospitalLocation' />
+                <input type="text" id='hospitalLocation' onChange={(e)=>{SetPatientLocation(e.target.value)}}/>
             </div>
             <div className="formGroup">
                 <label htmlFor="standeeName">
                     Standee Name :
                 </label>
-                <input type="text" id='standeeName' />
+                <input type="text" id='standeeName'  onChange={(e)=>{SetStandeeName(e.target.value)}}/>
             </div>
             <div className="formGroup">
                 <label htmlFor="standeeNumber">
                     Standee Mobile Number :
                 </label>
-                <input type="text" id='standeeNumber' inputMode='number'/>
+                <input type="text" id='standeeNumber' inputMode='number' onChange={(e)=>{SetStandeeNumber(e.target.value)}}/>
             </div>
-            <div className="formGroup">
+            {/* <div className="formGroup">
                 <span id='otpField'>
                 <label htmlFor="otp">
                     Otp : 
@@ -65,9 +86,9 @@ function RequestBlood() {
                 <button className="primaryButton">Verify</button>
                 </span>
                 <input type="text" id='otp' />
-            </div>
-            <input type="submit" className="primaryButton searchButton" value="Search" />
-        </form>
+            </div> */}
+            <button  className="primaryButton searchButton" onClick={handleClick}>Search</button>
+        </div>
     </div>
   )
 }
