540678f1b63543ca4c7b811277d883efdf709030
aswnss-m <aswnss.m@gmail.com>
2023-05-03 21:01:34 +0530

    avoids getting -email and -password

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user.model');
 
 router.route('/').get((req, res) => {
-  User.find()
+  User.find().select('-email -password') // exclude email and password fields
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err));
 });
