36c559be1308a3cf65bbb1182a2c34e01a887468
aswnss-m <aswnss.m@gmail.com>
2023-05-04 10:01:23 +0530

    login function updated

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,22 @@ import SearchResult from "./Pages/SearchResult";
 import Profile from "./Pages/Profile";
 import Login from "./Pages/Login";
 import Admin from "./Pages/Admin";
+import { useState } from "react";
 // import Footer from "./Components/Footer";
 function App() {
+  const [islogged, setIslogged] = useState(localStorage.getItem('isAuth'));
+
+const handleLogin = (isLoggedIn) => {
+  setIslogged(isLoggedIn);
+}
+
+const handleLogout = () => {
+  setIslogged(false);
+}
   return (
+
     <div className="app">
-      <Navbar />
+      <Navbar onLogout={handleLogout}/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<RequestBlood />} />
@@ -20,7 +31,7 @@ function App() {
         <Route path="/emergency" element={<EmergencyPage />} />
         <Route path="/search/:bloodgroup/:location" element={<SearchResult />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login onLogin={handleLogin}/>} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
       {/* <Footer /> */}
--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import "../Assets/Navbar.css";
-import {Link} from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import '../Assets/Navbar.css';
+import { Link } from 'react-router-dom';
 
+function Navbar({ onLogout }) {
+  const [islogged, setIslogged] = useState(localStorage.getItem('isAuth'));
 
-function Navbar() {
+  const localStorageItem = localStorage.getItem('isAuth');
+
+  useEffect(() => {
+    setIslogged(localStorageItem);
+  }, [localStorageItem]);
+
   return (
     <div className="navbar">
-        <span className='navbarLogo whiteText'>
+      <span className="navbarLogo whiteText">
         <h1>
-            <Link to='/' id='navbarLogo'>
-              Blood Bank
-            </Link>
-            </h1>
-        </span>
-        <span className="navbarLinks">
-            <Link className='darkAccent' to={'#'}>Contact</Link>
-            <Link to='/login'>Login</Link>
-        </span>
+          <Link to="/" id="navbarLogo">
+            Blood Bank
+          </Link>
+        </h1>
+      </span>
+      <span className="navbarLinks">
+        <Link className="darkAccent" to={'#'}>
+          Contact
+        </Link>
+        {islogged ? (
+          <Link to={'/'} onClick={() => {
+            setIslogged(false);
+            onLogout(false);
+            }}>
+            Logout
+          </Link>
+        ) : (
+          <Link to="/login">Login</Link>
+        )}
+      </span>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
